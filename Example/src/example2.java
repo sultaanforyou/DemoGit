c68a8ef16b23a4fd363198f1ba7353bdaf7f6aef
Prince <princechauhan5242@gmail.com>
2023-10-30 15:28:51 +0530

    first commit

---
+++ Example/src/example2.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class example2 {
+
+	@Test
+	public void check()
+	{
+		System.out.println("hello");
+	}
+	
+	@Test
+	public void check1()
+	{
+		System.out.println("testing frame work");
+	}
+}
